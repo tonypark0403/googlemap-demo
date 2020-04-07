946b2c5b87eb70f5014efb9465a78e7b980b4d32
Tony Park <tonypark0403@gmail.com>
2020-04-06 21:09:43 -0400

    added marker from data

--- src/WrappedMap.js
+++ src/WrappedMap.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
+import {
+  GoogleMap,
+  withScriptjs,
+  withGoogleMap,
+  Marker,
+} from 'react-google-maps';
+import * as parksData from './data/skateboard-parks.json';
 
 function Map() {
   return (
     <GoogleMap
       defaultZoom={10}
       defaultCenter={{ lat: 45.421532, lng: -75.697189 }}
-    />
+    >
+      {parksData.features.map((park) => (
+        <Marker
+          key={park.properties.PARK_ID}
+          position={{
+            lat: park.geometry.coordinates[1],
+            lng: park.geometry.coordinates[0],
+          }}
+        />
+      ))}
+    </GoogleMap>
   );
 }
 
-const WrappedMap = withScriptjs( withGoogleMap( Map ));
+const WrappedMap = withScriptjs(withGoogleMap(Map));
 export default WrappedMap;
